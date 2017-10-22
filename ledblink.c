1e1b708bd0a2807484619037f637c4d7fe98de88
John Cottrell <jcottrell@yahoo.com>
2017-10-22 21:36:10 +0000

    formatted ledblink.c code

--- ledblink.c
+++ ledblink.c
@@ -1,23 +1,27 @@
+/*
+   ledblink.c
+   blinks a built-in (user) led on the BBB
+   10/2017 John Cottrell
+*/
+
 #include<stdio.h>
 
 int main()
 {
-        FILE *LEDHandler=NULL;
-        char *LEDPath="/sys/class/leds/beaglebone:green:usr0/brightness";
-        
-        while(1) {
-        if(LEDHandler=fopen(LEDPath,"r+")){
-                fwrite("1", 1, 1,LEDHandler);
-                fclose(LEDHandler);
-        }
-        sleep(1);
-        if(LEDHandler=fopen(LEDPath,"r+")){
-                fwrite("0", 1, 1,LEDHandler);
-                fclose(LEDHandler);
-        sleep(1);
-        }
-        }
+   FILE *LEDHandler=NULL;
+   char *LEDPath="/sys/class/leds/beaglebone:green:usr0/brightness";
 
-        return 0;
+   while(1) {
+     if(LEDHandler=fopen(LEDPath,"r+")){
+       fwrite("1", 1, 1,LEDHandler);
+       fclose(LEDHandler);
+     }
+     sleep(1);
+     if(LEDHandler=fopen(LEDPath,"r+")){
+       fwrite("0", 1, 1,LEDHandler);
+       fclose(LEDHandler);
+     }
+     sleep(1);
+   }
+   return 0;
 }
-
